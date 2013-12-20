e9e3404eead0a649851d5f76e2d06f3f8da66df4
Orphée Religieux <orphee@complex.ch>
2013-12-20 15:11:02 +0100

    Init mprj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mproj::Application.config.secret_token = 'bd6f7205c5acfb5f3b1617fb484815e3269393f180ff1f3843ffb067192aad04eb713e91dfde4e9d20e588888d26ec2a0b0d7398b4a2f367468bef1410983442'
